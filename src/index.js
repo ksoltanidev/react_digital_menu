fb6bdc6db212d94e923ef3dad2c504a90b5b5a4e
ksoltanidev <ksoltani.pro@gmail.com>
2021-06-30 21:15:31 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ReactDOM from 'react-dom';
+import Cursor from './components/cursor/cursor';
+import CursorContextProvider from './components/cursorContext/cursorContext';
+import Menu from './components/menu/Menu';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-
-import Aboutme from './pages/Aboutme/Aboutme';
-import Portofolio from './pages/Portofolio/Portofolio';
-import Contacts from './pages/Contacts/Contacts';
-import Freestyle from './pages/Freestyle/Freestyle';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,25 +13,13 @@ ReactDOM.render(
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       <Switch>
-
-        <Route path="/aboutme">
-          <Aboutme />
-        </Route>
-
-        <Route path="/portofolio">
-          <Portofolio />
-        </Route>
-
-        <Route path="/contacts">
-          <Contacts />
-        </Route>
-
-        <Route path="/freestyle">
-          <Freestyle />
-        </Route>
-
         <Route path="/">
-          <Aboutme />
+          <CursorContextProvider>
+            <div className={"homeContainer"}>
+              <Cursor />
+              <Menu />
+            </div>
+          </CursorContextProvider>
         </Route>
 
       </Switch>
@@ -42,4 +27,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-reportWebVitals();
